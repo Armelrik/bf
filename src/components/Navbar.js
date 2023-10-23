f0239feffa93957dd0ff5df5c87f50c0c5dc79f4
Armelrik <armelrik@hotmail.com>
2023-10-23 08:52:44 +0000

    Comit1

---
+++ src/components/Navbar.js
@@ -0,0 +1,41 @@
+import './NavbarStyles.css';
+import { Component } from 'react';
+// import { Link } from 'react-router-dom';
+import { MenuItems } from './MenuItems';
+
+class Navbar extends Component {
+
+  state = { clicked: false};
+  handledClick = () => {
+    this.setState({ clicked: !this.state.clicked })
+  }
+
+  render(){
+    return(
+      <nav className="NavbarItems">
+        <h1 className="navbar-logo">Ambassade</h1>
+
+        <div className="menu-icons" onClick={this.handledClick}>
+          <i className={this.state.clicked ? "fas fa-times"
+            : "fas fa-bars" }></i>
+        </div>
+
+        <ul className={this.state.clicked ? "nav-menu active" :
+            "nav-menu" }>
+          { MenuItems.map((item, index) => {
+            return(
+              <li key={index}>
+                <a className={item.cName} href={item.url}>
+                  <i class={item.icon}></i>{item.title}</a>
+              </li>
+            )
+          })}
+          <button>Sign up</button>
+        </ul>
+      </nav>
+    );
+  }
+}
+
+export default Navbar;
+
