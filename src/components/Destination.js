6e6aa0ba6a00d2e4b9d00d3d0c68cc2ea1151a54
armelrik <armelrik@hotmail.com>
2024-04-22 14:32:11 +0000

    last

--- src/components/Destination.js
+++ src/components/Destination.js
@@ -3,11 +3,16 @@ import './DestinationStyles.css';
 import DestinationData from './DestinationData';
 import Img1 from "../assets/istockphoto-467228674-1024x1024.jpg";
 import Img2 from "../assets/istockphoto-1301083980-1024x1024.jpg";
-import Img3 from '../assets/istockphoto-1288874565-1024x1024.jpg';
+import Img3 from '../assets/imgs/img15.jpeg'
 import Img4 from '../assets/istockphoto-515599896-1024x1024.jpg';
+
+// import Img3 from '../assets/istockphoto-1288874565-1024x1024.jpg';
+// import Img4 from '../assets/istockphoto-515599896-1024x1024.jpg';
+
 import Img5 from "../assets/amb.jpeg";
 import Img6 from "../assets/Drapeau-bf.gif";
 import { useTranslation } from "react-i18next";
+
 
 const Destination = () => {
   const { t } = useTranslation();
