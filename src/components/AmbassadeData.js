e03e0d0860e6a21681fa59f870bee0b5f78dc94c
armelrik <armelrik@hotmail.com>
2024-08-07 18:29:18 +0000

    Aug

--- src/components/AmbassadeData.js
+++ src/components/AmbassadeData.js
@@ -2,16 +2,16 @@ import React from 'react'
 import './AmbassadorUsStyles.css';
 // import Img2 from "../assets/ambasador.jpeg";
 import Img1 from "../assets/amb.jpeg";
-// import { useTranslation } from 'react-i18next';
+import { useTranslation } from 'react-i18next';
 
 function AmbassadeData() {
-//   const { t } = useTranslation();
+const { t } = useTranslation();
   return (
     <div className='ambassador_container'>
       <img src={Img1} alt='ambassador' />
       <div className='ambassador_text'>
-        {/* <h1>{t('ambassador_head')}</h1> */}
-        <h1>Ambassade du Burkina-Faso aux Émirats Arabes Unis</h1>
+        <h1>{t('title_message')}</h1>
+        {/* <h1>Ambassade du Burkina-Faso aux Émirats Arabes Unis</h1> */}
         {/* <p>{t('ambassador_text')}</p> */}
         <p>
             </p>
